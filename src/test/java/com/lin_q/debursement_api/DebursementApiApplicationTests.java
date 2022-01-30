c26288fd58512ff811780d07a4035b42b9f1c158
Serge KOUAME <tyrelserge@outlook.fr>
2022-01-30 21:22:17 +0000

    Start projet

---
+++ src/test/java/com/lin_q/debursement_api/DebursementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lin_q.debursement_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebursementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
